359579a23791950a689bf515d162f9afafc361b8
Josh Long <josh@joshlong.com>
2022-02-14 12:01:17 -0800

    cfp

--- src/main/java/com/example/cfpnotifications/CfpNotificationsApplication.java
+++ src/main/java/com/example/cfpnotifications/CfpNotificationsApplication.java
@@ -2,6 +2,7 @@ package com.example.cfpnotifications;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -15,16 +16,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
 
 @SpringBootApplication
@@ -42,16 +39,16 @@ public class CfpNotificationsApplication {
 
 }
 
+@Slf4j
+@RequiredArgsConstructor
+class ChromeClient {
 
-/**
- * the possibility is: Document doc = PhantomJsUtils.renderPage(Jsoup.parse(yourSource))
- */
-
-record ChromeClient(File chromeDriverBinary) {
+    private final File chromeDriverBinary;
 
     @SneakyThrows
     Document render(URL url, Predicate<WebDriver> pageReadyWebDriverPredicate) {
-        System.setProperty("webdriver.chrome.driver", this.chromeDriverBinary.getAbsolutePath());
+        System.setProperty("webdriver.chrome.driver", chromeDriverBinary.getAbsolutePath());
+
         var headless = false;
         var options = new ChromeOptions()
                 .setHeadless(headless)
@@ -60,11 +57,13 @@ record ChromeClient(File chromeDriverBinary) {
         try {
             driver.get(url.toString());
             var webDriverWait = new WebDriverWait(driver, 30);
+            log.info("bef for WebDriver");
             webDriverWait.until(pageReadyWebDriverPredicate::test);
+            log.info("aft for WebDriver");
             return Jsoup.parse(driver.getPageSource());
         }//
         finally {
-            //  driver.quit();
+            driver.quit();
         }
     }
 }
@@ -73,7 +72,6 @@ record ChromeClient(File chromeDriverBinary) {
 record Address(String addressLocality, String addressCountry) {
 }
 
-/*location={@type=Place, address={@type=PostalAddress, addressLocality=null, addressCountry=null}, name=null, null}*/
 record Location(Address address, String name) {
 }
 
@@ -89,19 +87,27 @@ record Event(Location location, EventAttendanceMode eventAttendanceMode, String 
 
 @Slf4j
 @Component
-record ConfsTechClientRunner(ObjectMapper objectMapper,
-                             ChromeClient client) {
+record ConfsTechRunner(ObjectMapper objectMapper, ChromeClient client) {
 
+    List<Event> read() throws Exception {
+        var cfpUrl = "https://confs.tech/?online=hybrid&topics=java";
+        var render = this.client.render(new URL(cfpUrl), d -> d.findElements(By.tagName("section")).size() > 0);
+        var scripts = render.getElementsByAttributeValue("type", "application/ld+json");
+        var events = scripts.stream().map(Element::html).map(this::parse).toList();
+        if (log.isDebugEnabled())
+            for (var e : events)
+                log.debug("event: " + e);
+        return events;
+    }
 
     @SneakyThrows
     private Event parse(String jsonLd) {
         var tr = new TypeReference<Map<String, Object>>() {
         };
         var eventMap = objectMapper.readValue(jsonLd, tr);
-        log.info(eventMap.toString());
         return new Event(
                 parseLocation(eventMap.get("location")),//
-                parseEventAttendanceMode(eventMap.get("eventAttendanceMode")),//
+                parseEventAttendanceMode((String) eventMap.get("eventAttendanceMode")),//
                 (String) eventMap.get("name"),//
                 parseDate((String) eventMap.get("startDate")),//
                 parseDate((String) eventMap.get("endDate")),//
@@ -109,27 +115,24 @@ record ConfsTechClientRunner(ObjectMapper objectMapper,
         );
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void fetch() throws Exception {
-        var cfpUrl = "https://confs.tech/?online=hybrid&topics=java";
-        var render = this.client.render(new URL(cfpUrl), d -> d.findElements(By.tagName("section")).size() > 0);
-        var scripts = render.getElementsByAttributeValue("type", "application/ld+json");
-        var events = scripts.stream().map(Element::html).map(this::parse).toList();
-
-    }
-
     private Address parseAddress(Object address) {
         if (address instanceof Map map) {
-            var addressResult = new Address(
+            return new Address(
                     (String) map.get("addressLocality"),
                     (String) map.get("addressCountry")
             );
-            return addressResult;
         }
         return null;
     }
 
     private EventAttendanceMode parseEventAttendanceMode(Object eatm) {
+        if (eatm != null) {
+            if (eatm instanceof String url) {
+                for (var eam : EventAttendanceMode.values())
+                    if (url.toLowerCase(Locale.ROOT).contains(eam.name().toLowerCase(Locale.ROOT)))
+                        return eam;
+            }
+        }
         return null;
     }
 
@@ -147,10 +150,10 @@ record ConfsTechClientRunner(ObjectMapper objectMapper,
         var yr = Integer.parseInt(parts[0]);
         var mo = Integer.parseInt(parts[1]);
         var day = Integer.parseInt(parts[2]);
-        var gc = new GregorianCalendar();
-        gc.set(GregorianCalendar.MONTH, mo);
-        gc.set(GregorianCalendar.YEAR, yr);
-        gc.set(GregorianCalendar.DATE, day);
-        return Date.from(gc.toInstant());
+        var gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.set(GregorianCalendar.MONTH, mo);
+        gregorianCalendar.set(GregorianCalendar.YEAR, yr);
+        gregorianCalendar.set(GregorianCalendar.DATE, day);
+        return Date.from(gregorianCalendar.toInstant());
     }
 }
